7c4628c2dd2ffc363717c58905c2b4d975b30fd6
Gallon Zhou <gallonzhou04@gmail.com>
2024-07-20 18:34:45 +1200

    Feat: Created Project Structure

---
+++ quacktube/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
+
+const app = express();
+
+app.get('/process-video', (req, res) => {
+  // Get the path of the input viedo file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  // Handle request body errors
+  if (!(inputFilePath || outputFilePath)) {
+    res.status(400).send('Bad Request: Missing File Path.');
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360')
+    .on('end', () => {
+      return res.status(200).send('Processing finished successfully.');
+    })
+    .on('error', (err) => {
+      console.log(`An error occurred: `);
+      return res.status(500).send(`Internal Server Error: ${err.emessage}`);
+    })
+    .save(outputFilePath);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Video processing service listening at http://localhost:${port}`);
+});
