198830b8a7ed56a5f4ce1379286f266f38ef413a
Gallon Zhou <gallonzhou04@gmail.com>
2024-07-25 22:39:53 +1200

    feat: Added Google Sign In Feature

--- quacktube-web-client/app/navbar/navbar.tsx
+++ quacktube-web-client/app/navbar/navbar.tsx
@@ -1,10 +1,25 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './navbar.module.css';
 import SignIn from './sign-in';
+import { onAuthStateChangedHelper } from '../firebase/firebase';
+import { useEffect, useState } from 'react';
+import { User } from 'firebase/auth';
 
 export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  });
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
@@ -12,8 +27,7 @@ export default function Navbar() {
           <Image width={90} height={20} src="/youtube-logo.svg" alt="YouTube Logo" />
         </span>
       </Link>
-      <h1>{process.env.NEXT_PUBLIC_BRUH}</h1>
-      <SignIn />
+      <SignIn user={user} />
     </nav>
   );
 }
