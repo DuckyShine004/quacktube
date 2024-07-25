e67500930d827feefae5d30051a2c196de6adcce
Gallon Zhou <gallonzhou04@gmail.com>
2024-07-25 19:17:15 +1200

    Feat: Added Firebase SDK

---
+++ quacktube-web-client/app/firebase/firebase.ts
@@ -0,0 +1,36 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: `${process.env.FIREBASE_AUTH_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  appId: `${process.env.FIREBASE_APP_ID}`,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's details.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/** Signs the user in.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Triggers a callback when the user's auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+  return onAuthStateChanged(auth, callback);
+}
